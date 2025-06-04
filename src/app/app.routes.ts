174058ad685592a75d236bb52f1c389a83d8a765
David E <deerhou@gmail.com>
2025-06-03 21:13:01 -0500

    Update app configuration and routing; remove placeholder HTML and add product routes. Enhance app setup with HTTP client and component input binding.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'products', loadComponent: () => import('@/products/pages/products-list/products-list')},
+    {path: 'product', loadComponent: () => import('@/products/pages/product-form/product-form')},
+    {path: 'product/:id', loadComponent: () => import('@/products/pages/product-form/product-form')},
+    {path: '**', redirectTo: 'products', pathMatch: 'full'}
+];
